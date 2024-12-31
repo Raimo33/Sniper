a7a85d7861c04165bb012f7185d5cab2b1dd2831
Claudio <claudio.raimondi@protonmail.com>
2024-12-31 01:19:14 +0100

    Add TODOs

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@
 #    By: craimond <claudio.raimondi@protonmail.c    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/12/29 17:07:44 by craimond          #+#    #+#              #
-#    Updated: 2024/12/30 16:52:57 by craimond         ###   ########.fr        #
+#    Updated: 2024/12/31 01:16:03 by craimond         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ FROM alpine:3.21
 
 SHELL ["/bin/ash", "-c"]
 
-RUN apk add make gcc libuv-dev curl-dev libwebsockets-dev nghttp2-dev ngtcp2-dev brotli-dev openssl-dev zlib-dev yyjson-dev jemalloc-dev
+RUN apk add make gcc libuv-dev curl-dev libwebsockets-dev nghttp2-dev ngtcp2-dev brotli-dev openssl-dev yyjson-dev jemalloc-dev
 
 COPY ./build/ /build/
 
@@ -22,6 +22,6 @@ WORKDIR /build
 RUN make
 
 #TODO Ed25519 Keys (per REST e FIX (https://developers.binance.com/docs/binance-spot-api-docs/rest-api/endpoint-security-type#ed25519-keys))
-ENV API_KEY API_SECRET
+ENV API_KEY API_SECRET CA_CERTS_PATH
 USER nobody
 ENTRYPOINT ["./arb"]
--- build/srcs/main.c
+++ build/srcs/main.c
@@ -6,18 +6,14 @@
 /*   By: craimond <claudio.raimondi@protonmail.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 17:07:42 by craimond          #+#    #+#             */
-/*   Updated: 2024/12/30 16:57:07 by craimond         ###   ########.fr       */
+/*   Updated: 2024/12/31 01:19:05 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <curl/curl.h>
-#include <nghttp2/nghttp2.h>
-#include <ngtcp2/ngtcp2.h>
 #include <libwebsockets.h>
 #include <brotli/decode.h>
 #include <brotli/encode.h>
-#include <zlib.h>
-#include <zstd.h>
 #include <uv.h>
 #include <openssl/ssl.h>
 #include <jemalloc/jemalloc.h>
@@ -35,6 +31,7 @@ int main(void)
 {
   const char *api_key = getenv("API_KEY");
   const char *api_secret = getenv("API_SECRET");
+  const char *ca_certs_path = getenv("CA_CERTS_PATH");
   char *rest_endpoints[] = {
     "https://api.binance.com",
     "https://api-gcp.binance.com",
@@ -48,18 +45,21 @@ int main(void)
   const char *ws_endpoint = "data-stream.binance.vision:443";
 
   if (!api_key || !api_secret)
-    panic("Missing API credentials in env");
-
+    return (puts_fd("API_KEY or API_SECRET not set", STDERR_FILENO), 1);
+  
+  CURL curl;
   uv_loop_t loop;
+
   uv_loop_init(&loop);
   setup_timers(&loop, endpoints);
-  setup_rest();
+  setup_rest(&curl, ca_certs_path);
   setup_ws();
   setup_fix();
   uv_run(loop, UV_RUN_DEFAULT);
   uv_stop(loop);
   uv_loop_close(&loop);
-
+  curl_easy_cleanup(&curl);
+  curl_global_cleanup();
   return 0;
 }
 
@@ -77,13 +77,36 @@ void setup_timers(uv_loop_t *loop, char *endpoints[])
   uv_timer_start(&logging_timer, log_stats, 0, LOGGING_INTERVAL);
 }
 
-void setup_rest() //TODO informazioni generiche di inizializzazione
+void setup_rest(CURL *curl, const char *ca_certs_path)
 {
+  if (curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK)
+    return (puts_fd(curl_easy_strerror(res), STDERR_FILENO), 1);
+
+  curl = curl_easy_init();
+  if (!curl)
+    return (puts_fd("Failed to initialize curl", STDERR_FILENO), 1);
+  
+  curl_version_info_data *info = curl_version_info(CURLVERSION_NOW);
+  if (info->features & CURL_VERSION_BROTLI)
+    curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "br");
+
+  curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, 
+    (info->features & CURL_VERSION_HTTP2) ? CURL_HTTP_VERSION_2_0 : CURL_HTTP_VERSION_1_1);
+
+  if (info->features & CURL_VERSION_SSL) #TODO error checking per curl_easy_setopt
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+    curl_easy_setopt(curl, CURLOPT_CAINFO, ca_certs_path);
+    curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_3);
+  else
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    
   //https://developers.binance.com/docs/binance-spot-api-docs/rest-api/account-endpoints (#TODO per settare grandezza ordini (tipo lotti), dal codice si setta solo il profilo di rischio)
   //https://developers.binance.com/docs/binance-spot-api-docs/rest-api/account-endpoints#query-commission-rates-user_data (per settare le commissioni per rendere i calcoli piu precisi in base alle commissioni dell'account)
 }
 
-void setup_ws() //TODO live price data
+void setup_ws(void) //TODO live price data
 {
   https://developers.binance.com/docs/binance-spot-api-docs/web-socket-streams
 }
