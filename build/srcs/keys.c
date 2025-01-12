ff84b62a6b1eda3db7265202fed257fd92b1a22e
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-01-12 10:26:34 +0100

    Add makefile rules for memory and cache checks

--- build/srcs/keys.c
+++ build/srcs/keys.c
@@ -6,7 +6,7 @@
 /*   By: craimond <claudio.raimondi@pm.me>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 19:01:43 by craimond          #+#    #+#             */
-/*   Updated: 2025/01/11 21:52:30 by craimond         ###   ########.fr       */
+/*   Updated: 2025/01/12 10:01:28 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void init_keys(ssl_data_t *const ssl_data, keys_t *const keys)
   const uint16_t key_fd = open(PRIVATE_KEY_PATH, O_RDONLY | O_CREAT | O_EXCL | O_NONBLOCK);
   const uint16_t api_fd = open(API_KEY_PATH, O_RDONLY | O_CREAT | O_EXCL | O_NONBLOCK);
 
-  if (keys_exist)
+  if (keys_exist) //(nmap??)
   {
     //TODO read asynchronously and fill keys (private and api)
     //TODO generate public key from private key and fill keys
