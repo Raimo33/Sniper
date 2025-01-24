a210a73c6d1710687a96638c931cad75c8612519
Claudio Raimondi <claudio.raimondi@protonmail.com>
2025-01-24 22:15:01 +0100

    Add fixed number operations

--- build/srcs/fixed_point.c
+++ build/srcs/fixed_point.c
@@ -6,10 +6,48 @@
 /*   By: craimond <claudio.raimondi@pm.me>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 19:10:57 by craimond          #+#    #+#             */
-/*   Updated: 2025/01/24 19:11:04 by craimond         ###   ########.fr       */
+/*   Updated: 2025/01/24 22:14:03 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/fixed_point.h"
 
-//TODO
+//TODO capire se implementare overflow detection
+//TODO reciprocal multiplication
+//TODO division by zero
+//TODO simd support
+
+static const int32_t powers_of_ten[] ALIGNED(64) = {
+  1,
+  10,
+  100,
+  1000,
+  10000,
+  100000,
+  1000000,
+  10000000,
+  100000000,
+  1000000000
+};
+
+static inline bool mul_overflow(const int64_t a, const int64_t b, int64_t *result);
+
+inline fixed_point_t add(const fixed_point_t a, const fixed_point_t b)
+{
+  return (fixed_point_t){.value = a.value + b.value, .precision = a.precision};
+}
+
+inline fixed_point_t sub(const fixed_point_t a, const fixed_point_t b)
+{
+  return (fixed_point_t){.value = a.value - b.value, .precision = a.precision};
+}
+
+inline fixed_point_t mul(const fixed_point_t a, const fixed_point_t b)
+{
+  return (fixed_point_t){a.value * b.value, a.precision + b.precision};
+}
+
+inline fixed_point_t div(const fixed_point_t a, const fixed_point_t b)
+{
+  return (fixed_point_t){a.value * powers_of_ten[a.precision] / b.value, a.precision};
+}
--- build/srcs/headers/fixed_point.h
+++ build/srcs/headers/fixed_point.h
@@ -6,7 +6,7 @@
 /*   By: craimond <claudio.raimondi@pm.me>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 19:01:52 by craimond          #+#    #+#             */
-/*   Updated: 2025/01/24 19:07:35 by craimond         ###   ########.fr       */
+/*   Updated: 2025/01/24 21:49:13 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 # define FIXED_POINT_H
 
 # include <stdint.h>
+# include <stdbool.h>
 
 # include "extensions.h"
 
 typedef struct
 {
-  int64_t integer;
-  uint32_t scale;
+  int64_t value;
+  uint8_t precision; //number of decimal places
 } fixed_point_t;
 
 inline fixed_point_t HOT ALWAYS_INLINE add(const fixed_point_t a, const fixed_point_t b);
